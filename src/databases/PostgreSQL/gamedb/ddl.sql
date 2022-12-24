4ecb2514a5034cb916e554912b37f97f322afeb6
Oğuz Karan <oguzkaran@csystem.org>
2022-12-24 22:44:54 +0300

    Update

--- src/databases/PostgreSQL/gamedb/ddl.sql
+++ src/databases/PostgreSQL/gamedb/ddl.sql
@@ -6,7 +6,8 @@
 -- CREATE TABLE COMMANDS
 
 create table players (       
-	nick_name character varying(50) primary key,
+	player_id serial primary key,
+	nick_name character varying(50) unique not null,
 	password character varying(50) check(length(password) >= 5)  not null,
 	first_name character varying(100) not null,
 	middle_name character varying(100),
@@ -16,7 +17,6 @@ create table players (
 	birth_date date not null
 );
 
-
 create table games (
 	game_id serial primary key,
 	name varchar(255) not null,
@@ -25,11 +25,12 @@ create table games (
 	is_available bool default(true) not null
 );
 
+
 -- One player play many games, One game is played by many players
 
 create table players_to_games (
 	player_to_game_id bigserial primary key,
-	nick_name character varying(50) references players(nick_name) not null,
+	player_id int references players(player_id) not null,
 	game_id int references games(game_id) not null,
 	start_date date default(current_date) not null
 );
@@ -41,6 +42,5 @@ create table players_to_games_play_data (
 	end_time timestamp,
 	score bigint default(0) not null
 );
-
 
 
