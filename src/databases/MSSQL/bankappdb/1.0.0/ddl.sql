5c160a1ff5ab7b1647b143832eeec3587f461908
Oğuz Karan <oguzkaran@csystem.org>
2023-02-04 23:28:06 +0300

    Update

--- src/databases/MSSQL/bankappdb/1.0.0/ddl.sql
+++ src/databases/MSSQL/bankappdb/1.0.0/ddl.sql
@@ -2,9 +2,18 @@
 	bankappdb 1.0.0
 -----------------------------------------------------------------------------------------------------------------------*/
 
+create database dbpd22_bankappdb
+
+go
+
+use dbpd22_bankappdb
+
+go
+
+
 create table marital_status (
-	marital_status_id serial primary key,
-	description varchar(100) not null
+	marital_status_id int primary key identity(1, 1),
+	description nvarchar(100) not null
 );
 
 insert into marital_status (description) values ('Single'), ('Married'), ('Divorced');
@@ -12,40 +21,40 @@ insert into marital_status (description) values ('Single'), ('Married'), ('Divor
 create table customers (
 	customer_number bigint primary key,
 	citizen_number char(36) unique not null,
-	first_name varchar(100) not null,
-	middle_name varchar(100),
-	last_name varchar(100) not null,
+	first_name nvarchar(100) not null,
+	middle_name nvarchar(100),
+	last_name nvarchar(100) not null,
 	birth_date date not null,
-	marital_status_id int references marital_status(marital_status_id) not null,
-	is_alive boolean default(true) not null,	
-	is_active boolean default(true) not null,	
-	is_personnel boolean not null,	
-	is_local boolean not null,	
+	marital_status_id int foreign key references marital_status(marital_status_id) not null,
+	is_alive bit default(1) not null,	
+	is_active bit default(1) not null,	
+	is_personnel bit not null,	
+	is_local bit not null,	
 );
 
 create table customer_to_addresses (
-	address_id serial primary key,
-	customer_number bigint references customers(customer_number) not null,
-	description varchar(512) not null
+	address_id int primary key identity(1, 1),
+	customer_number bigint foreign key references customers(customer_number) not null,
+	description nvarchar(512) not null
 );
 
 create table phone_types (
-	phone_type_id serial primary key, 
-	description varchar(50) not null
+	phone_type_id int primary key identity(1, 1), 
+	description nvarchar(50) not null
 );
 
 insert into phone_types (description) values ('GSM'), ('WORK'), ('HOME');
 
 create table customer_to_phones (
-	phone_id serial primary key,
-	customer_number bigint references customers(customer_number) not null,
-	phone_type_id int references phone_types(phone_type_id) not null,
+	phone_id int primary key identity(1, 1),
+	customer_number bigint foreign key references customers(customer_number) not null,
+	phone_type_id int foreign key references phone_types(phone_type_id) not null,
 	number char(14) not null
 );
 
 create table card_types (
-	card_type_id serial primary key,
-	description varchar(30) not null
+	card_type_id int primary key identity(1, 1),
+	description nvarchar(30) not null
 );
 
 insert into card_types (description) values ('Visa'), ('Master'), ('Amex'); 
@@ -53,11 +62,10 @@ insert into card_types (description) values ('Visa'), ('Master'), ('Amex');
 
 create table cards (
 	card_number char(16) primary key,
-	card_type_id int references card_types(card_type_id) not null,
-	customer_number bigint references customers(customer_number) not null,
+	card_type_id int foreign key references card_types(card_type_id) not null,
+	customer_number bigint foreign key references customers(customer_number) not null,
 	expiry_month int check(1 <= expiry_month and expiry_month <= 12) not null,
 	expiry_year int not null, 
 	security_code char(4) not null	
 );
-
 
