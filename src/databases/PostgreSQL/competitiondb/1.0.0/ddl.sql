b2272c67d98993e5f312a7f03dccaf197a78702f
Oğuz Karan <oguzkaran@csystem.org>
2023-04-16 10:34:30 +0300

    Update

--- src/databases/PostgreSQL/competitiondb/1.0.0/ddl.sql
+++ src/databases/PostgreSQL/competitiondb/1.0.0/ddl.sql
@@ -27,14 +27,12 @@
 
     4. TODO: Bir önceki SP'yi tablo döndüren fonksiyon olarak yazınız. Soruya ilişkin önemli bilgileri döndürünüz
 -----------------------------------------------------------------------------------------------------------------------*/
-create database competitionappdb
-
-use competitionappdb
+create database competitionappdb;
 
 create table levels (
-    level_id int primary key identity(1, 1),
-    description nvarchar(100)
-)
+    level_id serial primary key,
+    description varchar(100)
+);
 
 insert into levels (description)
 values
@@ -42,97 +40,18 @@ values
 ('Seviye 6'), ('Seviye 7'), ('Seviye 8'), ('Seviye 9'), ('Seviye 10')
 
 create table questions (
-    question_id int primary key identity(1, 1),
-    description nvarchar(max) not null,
-    level_id int foreign key references levels(level_id) not null,
+    question_id serial primary key,
+    description varchar(1024) not null,
+    level_id int references levels(level_id) not null,
     answer_index int not null
 )
 
 create table options (
-    option_id int primary key identity(1, 1),
-    description nvarchar(max) not null,
-    question_id int foreign key references questions(question_id)
+    option_id serial primary key,
+    description varchar(512) not null,
+    question_id int references questions(question_id)
 )
 
-create view v_get_random
-as
-select rand() as random
-
-drop procedure sp_get_random_question_id
-
-create procedure sp_get_random_question_id(@question_id int output, @answer_index int output, @status bit output)
-as
-begin
-    -- TODO: Transaction safe hale getiriniz
-    declare crs_questions cursor scroll for select question_id from questions
-    open crs_questions
-
-    declare @bound int = (select count(*) from questions) + 1
-    declare @min int = 1
-    declare @index int = floor(rand() * (@bound - @min) + @min)
-
-    fetch absolute @index from crs_questions into @question_id
-
-    if @@fetch_status = 0
-        set @answer_index = (select answer_index from questions where question_id = @question_id)
-
-    if @@fetch_status = 0
-        set @status = 1
-    else
-        set @status = 0
-
-    close crs_questions
-    deallocate crs_questions
-end
 
 
-create procedure sp_get_random_question_id_by_level_id(@level_id int, @question_id int output, @answer_index int output, @status bit output)
-as
-begin
-    -- TODO: Transaction safe hale getiriniz
-    declare crs_questions cursor scroll for select question_id from questions where level_id = @level_id
-    open crs_questions
-
-    declare @bound int = (select count(*) from questions where level_id = @level_id) + 1
-    declare @min int = 1
-    declare @index int = floor(rand() * (@bound - @min) + @min)
-
-    fetch absolute @index from crs_questions into @question_id
-
-    if @@fetch_status = 0
-        set @answer_index = (select answer_index from questions where question_id = @question_id)
-
-    if @@fetch_status = 0
-        set @status = 1
-    else
-        set @status = 0
-
-    close crs_questions
-    deallocate crs_questions
-end
-
-
-declare @status bit
-declare @question_id int
-declare @answer_index int
-
-exec sp_get_random_question_id @question_id output, @answer_index output, @status output
-
-if @status = 1
-    select * from questions q inner join options o on q.question_id = o.question_id where o.question_id = @question_id
-else
-    select 'No question yet'
-
-------------------------------------------------------------------------------------------------------------------------
-
-declare @status bit
-declare @question_id int
-declare @answer_index int
-
-exec sp_get_random_question_id_by_level_id 1, @question_id output, @answer_index output, @status output
-
-if @status = 1
-    select * from questions q inner join options o on q.question_id = o.question_id where o.question_id = @question_id
-else
-    select 'No question yet'
 
